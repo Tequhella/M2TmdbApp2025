af5459fdb3b1f30f663484b0aab9271430809ba2
lpastyle <lpastorelli@lpastyle.com>
2025-03-18 16:40:26 +0100

    Added onMeasure

--- app/src/main/java/com/example/m2tmdbapp2025/ScoreGaugeView.kt
+++ app/src/main/java/com/example/m2tmdbapp2025/ScoreGaugeView.kt
@@ -4,10 +4,13 @@ import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
+import android.graphics.Typeface
 import android.graphics.drawable.Drawable
 import android.text.TextPaint
 import android.util.AttributeSet
 import android.view.View
+import kotlin.math.abs
+import kotlin.math.min
 
 
 class ScoreGaugeView @JvmOverloads constructor(
@@ -23,6 +26,17 @@ class ScoreGaugeView @JvmOverloads constructor(
     private var scoreLabel:String?
     private var scoreColor:Int
 
+    // Paint styles used for rendering are initialized here to improve performance,
+    // since onDraw() is called for every screen refresh.
+    private val paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        textSize = context.resources.getDimension(R.dimen.score_gauge_view_label)
+        typeface = Typeface.create(null as String?, Typeface.BOLD)
+    }
+
+    // compute dimensions to be used for drawing text
+    private val fontMetrics = paint.fontMetrics
+    private val textHeight = abs(paint.fontMetrics.ascent + fontMetrics.descent)
+
     init {
         val typedArray = context.obtainStyledAttributes(attrs, R.styleable.ScoreGaugeView)
         scoreMax = typedArray.getFloat(R.styleable.ScoreGaugeView_scoreMax, 100f)
@@ -31,6 +45,36 @@ class ScoreGaugeView @JvmOverloads constructor(
         if (scoreLabel == null) scoreLabel = if (isInEditMode) context.getString(R.string.no_label) else ""
         scoreColor = typedArray.getColor(R.styleable.ScoreGaugeView_scoreColor, Color.GREEN)
         typedArray.recycle()
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        // super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+
+        // requested width and height
+        val reqWidth = MeasureSpec.getSize(widthMeasureSpec)
+        val reqHeight = MeasureSpec.getSize(heightMeasureSpec)
+
+        // your choice
+        val desiredWidth: Int = reqWidth
+        val desiredHeight: Int = (textHeight * 2.5).toInt()
+
+        val width = when (MeasureSpec.getMode(widthMeasureSpec)) {
+            MeasureSpec.EXACTLY -> reqWidth
+            MeasureSpec.UNSPECIFIED -> desiredWidth
+            else -> min(reqWidth, desiredWidth) // AT_MOST condition
+        }
+        val height = when (MeasureSpec.getMode(heightMeasureSpec)) {
+            MeasureSpec.EXACTLY -> reqHeight
+            MeasureSpec.UNSPECIFIED -> desiredHeight
+            else -> min(reqHeight, desiredHeight) // AT_MOST condition
+        }
+
+        // set the width and the height of the view
+        if (isInEditMode) {
+            setMeasuredDimension(desiredWidth, desiredHeight)
+        } else {
+            setMeasuredDimension(width, height)
+        }
     }
 
 }
