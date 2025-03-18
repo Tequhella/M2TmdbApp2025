3079a46ee5cabcb727bba7c05abb409e76fd0252
lpastyle <lpastorelli@lpastyle.com>
2025-03-18 15:14:06 +0100

    Started ScoreGaugeView implementation

--- app/src/main/java/com/example/m2tmdbapp2025/ScoreGaugeView.kt
+++ app/src/main/java/com/example/m2tmdbapp2025/ScoreGaugeView.kt
@@ -9,148 +9,28 @@ import android.text.TextPaint
 import android.util.AttributeSet
 import android.view.View
 
-/**
- * TODO: document your custom view class.
- */
-class ScoreGaugeView : View {
 
-    private var _exampleString: String? = null // TODO: use a default from R.string...
-    private var _exampleColor: Int = Color.RED // TODO: use a default from R.color...
-    private var _exampleDimension: Float = 0f // TODO: use a default from R.dimen...
+class ScoreGaugeView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : View(context, attrs, defStyleAttr) {
+    private val LOGTAG = ScoreGaugeView::class.simpleName
 
-    private lateinit var textPaint: TextPaint
-    private var textWidth: Float = 0f
-    private var textHeight: Float = 0f
+    // hold XML custom attributes
+    private var scoreMax:Float
+    private var scoreValue:Float
+    private var scoreLabel:String?
+    private var scoreColor:Int
 
-    /**
-     * The text to draw
-     */
-    var exampleString: String?
-        get() = _exampleString
-        set(value) {
-            _exampleString = value
-            invalidateTextPaintAndMeasurements()
-        }
-
-    /**
-     * The font color
-     */
-    var exampleColor: Int
-        get() = _exampleColor
-        set(value) {
-            _exampleColor = value
-            invalidateTextPaintAndMeasurements()
-        }
-
-    /**
-     * In the example view, this dimension is the font size.
-     */
-    var exampleDimension: Float
-        get() = _exampleDimension
-        set(value) {
-            _exampleDimension = value
-            invalidateTextPaintAndMeasurements()
-        }
-
-    /**
-     * In the example view, this drawable is drawn above the text.
-     */
-    var exampleDrawable: Drawable? = null
-
-    constructor(context: Context) : super(context) {
-        init(null, 0)
+    init {
+        val typedArray = context.obtainStyledAttributes(attrs, R.styleable.ScoreGaugeView)
+        scoreMax = typedArray.getFloat(R.styleable.ScoreGaugeView_scoreMax, 100f)
+        scoreValue = typedArray.getFloat(R.styleable.ScoreGaugeView_scoreValue, 75f)
+        scoreLabel = typedArray.getString(R.styleable.ScoreGaugeView_scoreLabel)
+        if (scoreLabel == null) scoreLabel = if (isInEditMode) context.getString(R.string.no_label) else ""
+        scoreColor = typedArray.getColor(R.styleable.ScoreGaugeView_scoreColor, Color.GREEN)
+        typedArray.recycle()
     }
 
-    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
-        init(attrs, 0)
-    }
-
-    constructor(context: Context, attrs: AttributeSet, defStyle: Int) : super(
-        context,
-        attrs,
-        defStyle
-    ) {
-        init(attrs, defStyle)
-    }
-
-    private fun init(attrs: AttributeSet?, defStyle: Int) {
-        // Load attributes
-        val a = context.obtainStyledAttributes(
-            attrs, R.styleable.ScoreGaugeView, defStyle, 0
-        )
-
-        _exampleString = a.getString(
-            R.styleable.ScoreGaugeView_exampleString
-        )
-        _exampleColor = a.getColor(
-            R.styleable.ScoreGaugeView_exampleColor,
-            exampleColor
-        )
-        // Use getDimensionPixelSize or getDimensionPixelOffset when dealing with
-        // values that should fall on pixel boundaries.
-        _exampleDimension = a.getDimension(
-            R.styleable.ScoreGaugeView_exampleDimension,
-            exampleDimension
-        )
-
-        if (a.hasValue(R.styleable.ScoreGaugeView_exampleDrawable)) {
-            exampleDrawable = a.getDrawable(
-                R.styleable.ScoreGaugeView_exampleDrawable
-            )
-            exampleDrawable?.callback = this
-        }
-
-        a.recycle()
-
-        // Set up a default TextPaint object
-        textPaint = TextPaint().apply {
-            flags = Paint.ANTI_ALIAS_FLAG
-            textAlign = Paint.Align.LEFT
-        }
-
-        // Update TextPaint and text measurements from attributes
-        invalidateTextPaintAndMeasurements()
-    }
-
-    private fun invalidateTextPaintAndMeasurements() {
-        textPaint.let {
-            it.textSize = exampleDimension
-            it.color = exampleColor
-            textWidth = it.measureText(exampleString)
-            textHeight = it.fontMetrics.bottom
-        }
-    }
-
-    override fun onDraw(canvas: Canvas) {
-        super.onDraw(canvas)
-
-        // TODO: consider storing these as member variables to reduce
-        // allocations per draw cycle.
-        val paddingLeft = paddingLeft
-        val paddingTop = paddingTop
-        val paddingRight = paddingRight
-        val paddingBottom = paddingBottom
-
-        val contentWidth = width - paddingLeft - paddingRight
-        val contentHeight = height - paddingTop - paddingBottom
-
-        exampleString?.let {
-            // Draw the text.
-            canvas.drawText(
-                it,
-                paddingLeft + (contentWidth - textWidth) / 2,
-                paddingTop + (contentHeight + textHeight) / 2,
-                textPaint
-            )
-        }
-
-        // Draw the example drawable on top of the text.
-        exampleDrawable?.let {
-            it.setBounds(
-                paddingLeft, paddingTop,
-                paddingLeft + contentWidth, paddingTop + contentHeight
-            )
-            it.draw(canvas)
-        }
-    }
 }
