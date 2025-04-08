a3002bf6a9acd9d99d9c3ebaa1d5b73cbc52304c
lpastyle <lpastorelli@lpastyle.com>
2025-04-08 12:33:25 +0200

    create notifications

--- app/src/main/java/com/example/m2tmdbapp2025/TmdbNotifications.kt
+++ app/src/main/java/com/example/m2tmdbapp2025/TmdbNotifications.kt
@@ -1,12 +1,22 @@
 package com.example.m2tmdbapp2025
 
+import android.app.NotificationManager
 import android.content.Context
+import androidx.core.app.NotificationCompat
 import com.example.m2tmdbapp2025.model.Person
 
 class TmdbNotifications {
      companion object {
          fun createPopularPersonNotification(context : Context, p : Person) {
              // Create notification
+             val notification = NotificationCompat.Builder(context, NOTIFICATION_CHANNEL_ID)
+                 .setSmallIcon(androidx.appcompat.R.drawable.btn_radio_off_mtrl)
+                 .setContentTitle(context.getString(R.string.notification_title))
+                 .setContentText(context.getString(R.string.notification_content, p.name.toString(), p.popularity))
+                 .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                 .build()
          }
+
+
      }
 }
