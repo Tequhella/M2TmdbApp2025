4e94ed3c2a88a391463fa9f6cc2a301201895331
lpastyle <lpastorelli@lpastyle.com>
2025-04-08 14:40:30 +0200

    doWork implemented

--- app/src/main/java/com/example/m2tmdbapp2025/TmdbDailyWorker.kt
+++ app/src/main/java/com/example/m2tmdbapp2025/TmdbDailyWorker.kt
@@ -1,13 +1,33 @@
 package com.example.m2tmdbapp2025
 
 import android.content.Context
+import android.util.Log
 import androidx.work.Worker
 import androidx.work.WorkerParameters
+import java.io.IOException
 
 class TmdbDailyWorker  (context: Context, params: WorkerParameters) : Worker(context, params) {
     private val LOGTAG = TmdbDailyWorker::class.simpleName
 
     override fun doWork(): Result {
-        return Result.success()
+        Log.d(LOGTAG,"doWork()")
+        val tmdbapi = ApiClient.instance.create(ITmdbApi::class.java)
+        val call = tmdbapi.getPopularPerson(TMDB_API_KEY, 1)
+        try {
+            val response = call.execute() // Synchronous call
+            if (response.isSuccessful) {
+                TmdbNotifications.createPopularPersonNotification(
+                    applicationContext,
+                    response.body()?.results!![0] // assume first array element has the best popularity
+                    )
+                return Result.success()
+            } else {
+                return Result.failure()
+            }
+
+        } catch (e : IOException) {
+            Log.e(LOGTAG,"call to getPopularPerson() failed with ${e.message}")
+            return Result.retry()
+        }
     }
 }
