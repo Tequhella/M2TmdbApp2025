2cac0e0775d803daa0d78baff49191955d767e15
lpastyle <lpastorelli@lpastyle.com>
2025-05-20 10:06:13 +0200

    Demo social bar fragment

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,6 +62,11 @@ dependencies {
     implementation(libs.androidx.work.runtime.ktx)
     implementation(libs.androidx.legacy.support.v4)
 
+    // Fragment + LifeCycle
+    implementation(libs.androidx.lifecycle.livedata.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.androidx.fragment.ktx)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
---
+++ app/src/main/java/com/example/m2tmdbapp2025/SocialBarViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.m2tmdbapp2025
+
+import androidx.lifecycle.ViewModel
+
+class SocialBarViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
