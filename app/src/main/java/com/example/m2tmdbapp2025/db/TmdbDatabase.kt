58a397dff3070bda958d05260a7fd46342933728
lpastyle <lpastorelli@lpastyle.com>
2025-05-23 10:30:26 +0200

    Added ROOM singleton init

---
+++ app/src/main/java/com/example/m2tmdbapp2025/db/TmdbDatabase.kt
@@ -0,0 +1,41 @@
+package com.example.m2tmdbapp2025.db
+
+import android.content.Context
+import android.util.Log
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+// Annotates class to be a Room Database with a table (entity) of the SocialBarEntity class
+@Database(entities = [SocialBarEntity::class], version = 1, exportSchema = false)
+abstract class TmdbDatabase : RoomDatabase() {
+
+    abstract fun socialBarDao(): SocialBarDao
+
+    companion object {
+        private val LOGTAG = TmdbDatabase::class.simpleName
+        // Singleton prevents multiple instances of database opening at the same time.
+        @Volatile
+        private var INSTANCE: TmdbDatabase? = null
+
+        fun getDatabase(
+            context: Context
+        ): TmdbDatabase {
+            Log.d(LOGTAG,"getDatabase()")
+            // if the INSTANCE is not null, then return it,
+            // if it is, then create the database
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    TmdbDatabase::class.java,
+                    "tmdb_database"
+                )
+                    //.allowMainThreadQueries()
+                    .build()
+                INSTANCE = instance
+                // return instance
+                instance
+            }
+        }
+    }
+}
