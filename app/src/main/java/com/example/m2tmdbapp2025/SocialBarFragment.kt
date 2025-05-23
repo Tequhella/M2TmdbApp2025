618c0e845151b911e01c67130b0b3de7b622bd06
lpastyle <lpastorelli@lpastyle.com>
2025-05-23 11:21:42 +0200

    Fix for LiveData

--- app/src/main/java/com/example/m2tmdbapp2025/SocialBarFragment.kt
+++ app/src/main/java/com/example/m2tmdbapp2025/SocialBarFragment.kt
@@ -10,25 +10,25 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
 import com.example.m2tmdbapp2025.databinding.FragmentSocialBarBinding
+import com.example.m2tmdbapp2025.db.SocialBarEntity
+import java.util.Locale
 
 class SocialBarFragment : Fragment() {
-    private val LOGTAG = SocialBarFragment::class.simpleName
-    private lateinit var binding : FragmentSocialBarBinding
-    private var cn : Int? = Color.LTGRAY
-    private var cs : Int? = Color.RED
 
     companion object {
-        fun newInstance() = SocialBarFragment()
+        fun newInstance() : SocialBarFragment = SocialBarFragment()
     }
 
-    //private val viewModel: SocialBarViewModel by viewModels()
-    private val viewModel by activityViewModels<SocialBarViewModel>()
+    private val LOGTAG = SocialBarFragment::class.simpleName
+    private lateinit var binding : FragmentSocialBarBinding
+    private var cn : Int = Color.LTGRAY
+    private var cs : Int = Color.RED
 
-    /*override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        // TODO: Use the ViewModel
-    }*/
+    // Using the activityViewModels() Kotlin property delegate from the
+    // fragment-ktx artifact to retrieve the ViewModel in the activity scope
+    private val viewModel by activityViewModels<SocialBarViewModel> {
+        SocialBarViewModelFactory(((requireContext() as MainActivity).application as TmdbApplication).socialBarDao)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -40,37 +40,44 @@ class SocialBarFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        cn = context?.getColor(R.color.ic_social_normal)
-        cs = context?.getColor(R.color.ic_social_selected)
+        cn = requireContext().getColor(R.color.ic_social_normal)
+        cs = requireContext().getColor(R.color.ic_social_selected)
 
         requireArguments().getString("sbfc_view_tag")?.let {
             val mapkey = it.toInt()
-            //Log.d(LOGTAG, "mapkey=$mapkey")
-
-            // set favorite button
-            val isFavorite = viewModel.isFavorite.getOrElse(mapkey,{false})
-            binding.favoriteIv.setColorFilter(if (isFavorite) cs!! else cn!!)
-            binding.favoriteIv.setOnClickListener {
-                viewModel.isFavorite.set(mapkey, !viewModel.isFavorite.getOrElse(mapkey,{false}))
-                binding.favoriteIv.setColorFilter(if (viewModel.isFavorite[mapkey] == true) cs!! else cn!! )
-            }
 
             // set like button
-            val nblikes = viewModel.nbLikes.getOrElse(mapkey, {0})
-            binding.nbLikeTv.setText(nblikes.toString())
-            val likeColor =  if (nblikes > 0 ) cs!! else cn!!
+            val nblikes = viewModel.nbLikes.value?.getOrElse(mapkey, {0}) ?:0
+            binding.nbLikeTv.text = String.format(Locale.getDefault(), "%d", nblikes)
+            val likeColor =  if (nblikes > 0 ) cs else cn
             binding.likeIv.setColorFilter(likeColor)
             binding.nbLikeTv.setTextColor(likeColor)
 
             binding.likeIv.setOnClickListener {
-                viewModel.nbLikes.set(mapkey,viewModel.nbLikes.getOrElse(mapkey, {0}) + 1)
-                binding.nbLikeTv.setText(viewModel.nbLikes[mapkey].toString())
-                binding.likeIv.setColorFilter(cs!!)
-                binding.nbLikeTv.setTextColor(cs!!)
+                var nbLikesNow = viewModel.nbLikes.value?.getOrElse(mapkey, {0}) ?: 0
+                val isFavorite = viewModel.isFavorite.value?.getOrElse(mapkey,{false}) ?: false
+                nbLikesNow++
+                viewModel.insert(SocialBarEntity(mapkey, isFavorite, nbLikesNow))
+                viewModel.nbLikes.value?.set(mapkey, nbLikesNow)
+                binding.nbLikeTv.text = String.format(Locale.getDefault(), "%d", nbLikesNow)
+                binding.likeIv.setColorFilter(cs)
+                binding.nbLikeTv.setTextColor(cs)
+            }
+
+            // set favorite button
+            val isFavorite = viewModel.isFavorite.value?.getOrElse(mapkey,{false}) ?: false
+            binding.favoriteIv.setColorFilter(if (isFavorite) cs else cn)
+            binding.favoriteIv.setOnClickListener {
+                var isFavoriteNow = viewModel.isFavorite.value?.getOrElse(mapkey,{false}) ?: false
+                val nbLikes = viewModel.nbLikes.value?.getOrElse(mapkey, {0}) ?: 0
+                isFavoriteNow = !isFavoriteNow
+                viewModel.insert(SocialBarEntity(mapkey, isFavoriteNow, nbLikes))
+                viewModel.isFavorite.value?.set(mapkey, isFavoriteNow)
+                binding.favoriteIv.setColorFilter(if (isFavoriteNow) cs else cn)
             }
 
             // set share button
-            binding.shareIv.setColorFilter(cn!!)
+            binding.shareIv.setColorFilter(cn)
             binding.shareIv.setOnClickListener {
                 Log.d(LOGTAG,"shared clicked for id=${mapkey}")
             }
