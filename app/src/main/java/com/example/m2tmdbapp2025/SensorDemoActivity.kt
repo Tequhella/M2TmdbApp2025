07008f727c9cf80cdb8999523dd1575ffea36291
lpastyle <lpastorelli@lpastyle.com>
2025-06-10 12:04:27 +0200

    sensor RV init

--- app/src/main/java/com/example/m2tmdbapp2025/SensorDemoActivity.kt
+++ app/src/main/java/com/example/m2tmdbapp2025/SensorDemoActivity.kt
@@ -5,16 +5,28 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.m2tmdbapp2025.databinding.ActivitySensorDemoBinding
 
 class SensorDemoActivity : AppCompatActivity() {
+    lateinit var binding: ActivitySensorDemoBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivitySensorDemoBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_sensor_demo)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // init sensor list recycler view
+        binding.sensorListRv.setHasFixedSize(true)
+        binding.sensorListRv.layoutManager = LinearLayoutManager(this)
+        //sensorListAdapter = SensorListAdapter(sensors, this)
+        //binding.sensorListRv.adapter = sensorListAdapter
+
+
     }
 }
