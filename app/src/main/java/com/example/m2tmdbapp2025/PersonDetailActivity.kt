de5e1a934b742029d75755ea1cf55864a91a8772
lpastyle <lpastorelli@lpastyle.com>
2025-06-10 10:09:01 +0200

    Added PersonDetailActivity

--- app/src/main/java/com/example/m2tmdbapp2025/PersonDetailActivity.kt
+++ app/src/main/java/com/example/m2tmdbapp2025/PersonDetailActivity.kt
@@ -5,16 +5,23 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.m2tmdbapp2025.databinding.ActivityPersonDetailBinding
 
 class PersonDetailActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityPersonDetailBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityPersonDetailBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_person_detail)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        binding.personIdTv.text = intent.extras?.getString(PERSON_ID_EXTRA_KEY)
     }
 }
